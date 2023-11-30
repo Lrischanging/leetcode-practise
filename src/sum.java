01d3155da08bf29a2a4dfd25930aef50bbe3adc0
v_ruvili <lr1667103623@163.com>
2023-11-30 15:40:09 +0800

    hash improve

--- src/sum.java
+++ src/sum.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class sum {
+    // 暴力解法，时间复杂度O(N²)
     public static int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++) {
             for(int j = i + 1; j < nums.length; j++) {
@@ -10,6 +12,18 @@ public class sum {
                     };
                 }
             }
+        }
+        return new int[0];
+    }
+
+    // 哈希表查找 target - num[1]，降低时间复杂度为O(N)
+    public static int[] twoSum1(int[] nums, int target) {
+        HashMap<Integer, Integer> hashtable = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{i, hashtable.get(target - nums[i])};
+            }
+            hashtable.put(nums[i], i);
         }
         return new int[0];
     }
