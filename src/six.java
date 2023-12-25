062f6093046bdda8355ed3a289a69dab7a8c5f60
v_ruvili <lr1667103623@163.com>
2023-12-25 18:50:46 +0800

    6. Z 字形变换

---
+++ src/six.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class six {
+    public static String convert(String s, int numRows) {
+        if(numRows == 1)
+            return s;
+
+        int curRow = 0;
+        boolean isDown = false;
+        int len = Math.min(s.length(), numRows);
+        String[] newArray = new String[len];
+        Arrays.fill(newArray, "");
+
+        for(int i=0; i<s.length(); i++) {
+            newArray[curRow] += s.substring(i, i+1);
+            if(curRow == 0 || curRow == numRows - 1) {
+                isDown = !isDown;
+            }
+            curRow += isDown ? 1: -1;
+        }
+
+        String result = "";
+        for(String row: newArray) {
+            result += row;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+        String res = convert(s, numRows);
+        System.out.println(res);
+    }
+}
